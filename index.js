d97cd179bee32bc49e7cdfd9165b698c7f13f0eb
Erik Baauw <erik.baauw@xs4all.nl>
2024-04-27 19:50:04 +0200

    Fix documentation

---
+++ index.js
@@ -0,0 +1,9 @@
+// hb-deconz-tools/index.js
+//
+// Homebridge deCONZ Tools.
+// Copyright © 2022-2024 Erik Baauw. All rights reserved.
+
+/** Homebridge deCONZ Tools.
+  *
+  * @module hb-deconz-tools
+  */
